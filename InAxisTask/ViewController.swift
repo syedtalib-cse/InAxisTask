07982a17dd8a9067da7cfcee4b8179563df15fa0
TalibIOS <goldsikkait13@gmail.com>
2022-12-29 11:17:58 +0530

    Initial Commit

---
+++ InAxisTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InAxisTask
+//
+//  Created by Talib on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
